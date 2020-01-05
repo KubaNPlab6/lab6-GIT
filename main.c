afa46c6f2ed90c7cb8906e398bb2c9110709eef8
KubaNPlab6 <59540745+KubaNPlab6@users.noreply.github.com>
2020-01-05 22:27:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 int main()
 {
 
-
+int main() {
+    int num1, num2;
+    char operator;
+    
+    printf("enter 1st number\n");
+    scanf("%d",&num1);
+    printf("enter 2nd number\n");
+    scanf("%d",&num2);
+    
+    printf("enter operator\n");
+    scanf(" %c",&operator);
 
 }
