89e6ea73bb4322986951ade1e2e22b2423c7b7d1
zbyszekrlab6 <59540751+zbyszekrlab6@users.noreply.github.com>
2020-01-06 20:14:44 +0100

    Create dzielenie.c

---
+++ dzielenie.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void dzielenie() {
+ 	int a, b, c;
+	printf("Podaj pierwsza liczbe: ");
+	scanf("%d", &a);
+	printf("Podaj druga liczbe: ");
+	scanf("%d", &b);
+ 	c=a/b;
+ 	printf("Wynik dzielenia %d / %d = %d", a, b, c);
+ }
